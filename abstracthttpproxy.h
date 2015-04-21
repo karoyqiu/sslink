3b3886e951f9456222cc66d32e5d8c8390c39323
karoyqiu <karoyqiu@gmail.com>
2015-04-21 20:27:27 +0800

    Add MEOW.

---
+++ abstracthttpproxy.h
@@ -0,0 +1,43 @@
+/*! ***********************************************************************************************
+ *
+ * file
+ * brief       The  class.
+ *
+ * version     0.1
+ * date        2015/4/21
+ *
+ * author      Roy QIU (karoyqiu@gmail.com)
+ * copyright   © 2015 Roy QIU.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * email to karoyqiu@gmail.com.
+ *
+ **************************************************************************************************/
+#pragma once
+
+#ifndef ABSTRACTHTTPPROXY_H
+#define ABSTRACTHTTPPROXY_H
+
+#include <QObject>
+
+class AbstractHttpProxy : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AbstractHttpProxy(QObject *parent = 0);
+    ~AbstractHttpProxy();
+
+signals:
+
+public slots:
+};
+
+#endif // ABSTRACTHTTPPROXY_H
